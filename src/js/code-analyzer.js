513a6e210250239e18961f04e92e34d6faf46a76
Sahar Hagbi <sahsahar@gmail.com>
2018-11-16 00:34:43 +0200

    Added function templates for all expression types

--- src/js/code-analyzer.js
+++ src/js/code-analyzer.js
@@ -4,4 +4,56 @@ const parseCode = (codeToParse) => {
     return esprima.parseScript(codeToParse, {loc: true});
 };
 
+function parse(exp){
+    return parseByType[exp.type](exp);
+}
+
+const parseByType ={
+    'FunctionDeclaration': parseFunction,
+    'VariableDeclaration': parseVarDec,
+    'ExpressionStatement': parseExp,
+    'AssignmentExpression': parseAssignExp,
+    'UpdateExpression': parseUpdateExp,
+    'WhileStatement': parseWhile,
+    'ForStatement': parseFor,
+    'IfStatement': parseIf,
+    'ReturnStatement': parseReturn
+};
+
+function parseFunction(){
+
+}
+
+function parseVarDec(){
+
+}
+
+function parseExp(){
+
+}
+
+function parseAssignExp(){
+
+}
+
+function parseUpdateExp(){
+
+}
+
+function parseWhile(){
+
+}
+
+function parseFor(){
+
+}
+
+function parseIf(){
+
+}
+
+function parseReturn(){
+
+}
+
 export {parseCode};
